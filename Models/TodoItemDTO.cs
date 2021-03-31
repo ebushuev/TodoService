98a46e6f1d03e5a4ccc22a6ae377a16bca65bb55
Jim J <buyanov.d.v@gmail.com>
2021-03-31 22:38:26 +0300

    dto markers added

--- Models/TodoItemDTO.cs
+++ Models/TodoItemDTO.cs
@@ -1,10 +1,16 @@
-﻿namespace TodoApi.Models
+﻿using System.Runtime.Serialization;
+
+namespace TodoApi.Models
 {
     #region snippet
+    [DataContract]
     public class TodoItemDTO
     {
+        [DataMember]
         public long Id { get; set; }
+        [DataMember]
         public string Name { get; set; }
+        [DataMember]
         public bool IsComplete { get; set; }
     }
     #endregion
