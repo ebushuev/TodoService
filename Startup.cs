dcc68c206a78c13bab9fbb90222b37d22501665f
Dmitriy Yudin <dmitriy.yudin@bbconsult.co.uk>
2021-05-22 08:48:13 +0500

    swagger setup

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using TodoApi.Models;
+using TodoApiDTO;
 
 namespace TodoApi
 {
@@ -30,6 +31,11 @@ namespace TodoApi
             services.AddDbContext<TodoContext>(opt =>
                opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo() { Title = "Todo API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +45,20 @@ namespace TodoApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            var swaggerOptions = new SwaggerOptions();
+            Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+
+            app.UseSwagger(option =>
+            {
+                option.RouteTemplate = swaggerOptions.JsonRoute;
+            });
+
+            app.UseSwaggerUI(option =>
+            {
+                option.SwaggerEndpoint(swaggerOptions.UIEndpoint, swaggerOptions.ApiDecription);
+                option.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
---
+++ SwaggerOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApiDTO
+{
+    public class SwaggerOptions
+    {
+        public string JsonRoute { get; set; }
+        public string ApiDecription { get; set; }
+        public string UIEndpoint { get; set; }
+    }
+}
